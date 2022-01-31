a56b0abbc74da25769e6379c43938389fce8937c
TheDarkGammers <bobafett7788@gmail.com>
2022-01-31 10:21:06 -0600

    test

--- webServer/Templog.py
+++ webServer/Templog.py
@@ -1,7 +1,12 @@
 from sensor_T import *
 import asyncio
+import time
 
 sT = sensor_T(None)
-T = sT.read()
-print(T)
-print("hello")
+
+
+for i in range(10):
+    T = sT.read()
+    print(i,T)
+    time.sleep(5)
+print("done")
