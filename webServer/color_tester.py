62dc0538a0fe3fda7d12f684f89bdd64ba819d63
TheDarkGammers <bobafett7788@gmail.com>
2022-02-01 10:30:11 -0600

    color

--- webServer/color_tester.py
+++ webServer/color_tester.py
@@ -4,8 +4,8 @@ from ledPixels import *
 
 ledPixels = ledPix
 
-    for i in range(0,10,5):
-        ledPix.setColor((255, 153, 51,))
+for i in range(0,10,5):
+    ledPix.setColor((255, 153, 51,))
 
  ledPix.setColor((0, 0, 0,))
 print("done")
