8ad3602e3fb52f8e7478a943ae327b77f72e9189
TheDarkGammers <bobafett7788@gmail.com>
2022-02-01 09:57:01 -0600

    led temp test

--- webServer/Templog.py
+++ webServer/Templog.py
@@ -1,14 +1,23 @@
 from sensor_T import *
 import asyncio
 import time
+import neopixel
+from ledPixels import *
+
 
 sT = sensor_T(None)
 
+ledPix = ledPixels(20, board.D18)
+
 with open('data.csv','w') as f:
 
-    for i in range(0,25,5):
+    for i in range(0,25,1):
         T = sT.read()
         print(i,T)
         f.write(f"{i},{T}\n")
         time.sleep(5)
+
+        if T <= 25:
+            ledPix setColor((0,0,255))
+
 print("done")
