b07dc7b5dfc3333811d2959a6643f4334459c720
TheDarkGammers <bobafett7788@gmail.com>
2022-02-01 10:43:40 -0600

    color

--- webServer/color_tester.py
+++ webServer/color_tester.py
@@ -1,14 +1,16 @@
+import asyncio
 import time
 import neopixel
 from ledPixels import *
 
 
-ledPixels = ledPix
+
 
 ledPix = ledPixels(20, board.D18)
 
-for i in range(0,10,5):
-    ledPix.setColor((255, 153, 51,))
+
+    for i in range(0,25,5):
+        ledPix.setColor((255, 153, 51,))
 
 
 ledPix.setColor((0, 0, 0,))
