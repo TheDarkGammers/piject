84a919dd1ad306da723c2664ba972bb4a04fc941
TheDarkGammers <bobafett7788@gmail.com>
2022-02-01 10:27:13 -0600

    color

---
+++ webServer/color_tester.py
@@ -0,0 +1,11 @@
+import time
+import neopixel
+from ledPixels import *
+
+ledPixels = ledPix
+
+    for i in range(0,10,5):
+        ledPix.setColor((255, 153, 51,))
+
+ ledPix.setColor((0, 0, 0,))
+print("done")
