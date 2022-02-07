89846f55596042ad745344c19e1a0254daee003a
TheDarkGammers <bobafett7788@gmail.com>
2022-02-07 13:30:47 -0600

    30

--- webServer/color_tester.py
+++ webServer/color_tester.py
@@ -17,7 +17,7 @@ ledPix = ledPixels(20, board.D18)
 
 with open('data.csv','w') as f:
 
-    for i in range(0,25,5):
+    for i in range(0,35,5):
         T = sT.read()
         print(i,T,F(T))
         f.write(f"{i},{T}\n")
