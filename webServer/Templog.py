6fb686cbc5dda5dc6f7cf1798898656ce273b178
TheDarkGammers <bobafett7788@gmail.com>
2022-02-07 14:15:02 -0600

    update

--- webServer/Templog.py
+++ webServer/Templog.py
@@ -23,14 +23,11 @@ with open('data.csv','w') as f:
         f.write(f"{i},{T}\n")
 
 
-        if T <= 20:
-            ledPix.setColor((0, 0, 255))
+        if T <= 12:
             ledPix.twoColors(6, col1=(0,0,255), col2=(0,0,0))
-        elif T >= 25:
-            ledPix.setColor((255, 0, 0,))
+        elif T >= 26:
             ledPix.twoColors(20, col1=(255, 0, 0), col2=(0,0,0))
         else:
-            ledPix.setColor((255, 40, 0,))
             ledPix.twoColors(12, col1=(255, 40, 0), col2=(0,0,0))
         time.sleep(5)
 
