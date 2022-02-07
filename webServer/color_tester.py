34044bbe5c5015ad9a5c1902c8f524ab7f40e4b9
TheDarkGammers <bobafett7788@gmail.com>
2022-02-07 13:27:15 -0600

    ohgod

--- webServer/color_tester.py
+++ webServer/color_tester.py
@@ -24,16 +24,37 @@ with open('data.csv','w') as f:
 
 
         if T >= 20:
-            ledPix.twoColors(1, col1=(0, 0, 255), col2=(0,0,0))
+            ledPix.twoColors(2, col1=(0, 0, 255), col2=(0, 0, 0))
 
         if T >= 21:
-            ledPix.twoColors(2, col1=(0, 0, 100), col2=(0,0,0))
+            ledPix.twoColors(4, col1=(225, 40, 20), col2=(0, 0, 0))
 
         if T >= 22:
-            ledPix.twoColors(3, col1=(225, 40, 0))
+            ledPix.twoColors(6, col1=(225, 40, 0), col2=(0, 0, 0))
 
         if T >= 23:
-            ledPIx.twoColors(3, col1=(225, 40, 0))
+            ledPix.twoColors(8, col1=(225, 40, 0), col2=(0, 0, 0))
+
+        if T >= 24:
+            ledPix.twoColors(10, col1=(225, 40, 0), col2=(0, 0, 0))
+
+        if T >= 25:
+            ledPix.twoColors(12, col1=(225, 10, 0), col2=(0, 0, 0))
+
+        if T >= 26:
+            ledPix.twoColors(14, col1=(225, 0, 0), col2=(0, 0, 0))
+
+        if T >= 27:
+            ledPix.twoColors(16, col1=(225, 0, 0), col2=(0, 0, 0))
+
+         if T >= 28:
+             ledPix.twoColors(18, col1=(225, 0, 0), col2=(0, 0, 0))
+
+        if T >= 29:
+            ledPix.twoColors(19, col1=(225, 0, 0), col2=(0, 0, 0))
+
+        if T >= 30:
+            ledPix.twoColors(20, col1=(225, 0, 0), col2=(0, 0, 0))
 
         time.sleep(5)
 
